7ce83eed94cf07eb3770a6a0342e46c31ff08516
Leonard <leonardglez12485@gmail.com>
2025-04-15 19:21:27 -0300

    Implement structural updates and optimizations across multiple modules

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,6 +1,21 @@
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar navbar-dark bg-dark-blue mb-4 px-4">
+        <span className="navbar-brand">
+            <i className="fas fa-calendar-alt">
+                &nbsp;
+                 Leonardo
+            </i>
+        </span>
+        <button 
+            className="btn btn-outline-danger"
+            onClick={() => console.log('Logout')}
+        >
+            <i className="fas fa-sign-out-alt"></i>
+            &nbsp;
+            <span>Salir</span>
+        </button>
+    </div>
   )
 }
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { addHours } from 'date-fns'
+import { Navbar } from '../'
+import { localizer, getMesagesES } from '../../helpers'
+import { useState } from 'react'
+
+
+const myEventsList = [{
+  title: 'Big Meeting',
+  note: 'Big Meeting',
+  start: new Date(),
+  end: addHours( new Date(), 2),
+  backgroundColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Leonardo'
+  }
+}]
 
 export const CalendarPage = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+  const onViewChange = (e) => {
+    localStorage.setItem('lastView', e);
+    setLastView(e);
+  }
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
-    <div>CalendarPage</div>
+    <>
+    <Navbar />
+    <Calendar
+      culture='es'
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      // style={{ height: 'calc(100vh - 80px)' }}
+      style={{ height: 500 }}
+      messages={getMesagesES()}
+      eventPropGetter= {eventStyleGetter}
+      onView={onViewChange}
+      view={lastView}
+       
+    />
+    </>
   )
 }
